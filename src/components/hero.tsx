b4c18ef036f4be0169b30b3065bdbcb8bf62dadc
Soumyanil Das <soumyanil666@gmail.com>
2023-07-13 17:30:27 +0530

    Fixed issue with welcome message

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
 
-  const [welcomeMessage, setWelcomeMessage] = React.useState('');
+  const [welcomeMessage, setWelcomeMessage] = React.useState('Hello');
   
   React.useEffect(() => {
     generateGreetings();
