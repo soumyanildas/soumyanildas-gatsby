8676d62daffe0da8a6da7bb57d14d7254570283f
Soumyanil Das <soumyanil666@gmail.com>
2023-07-13 17:35:23 +0530

    Checking state performance on prod

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Hero = () => {
 
-  const [welcomeMessage, setWelcomeMessage] = React.useState('Hello');
-  
+  const [welcomeMessage, setWelcomeMessage] = React.useState('');
+
   React.useEffect(() => {
     generateGreetings();
   }, []);
@@ -16,11 +16,11 @@ const Hero = () => {
     const randomIndex = Math.floor(Math.random() * greetings.length);
     setWelcomeMessage(greetings[randomIndex]);
   };
-  
+
   return (
     <div className="container mx-auto px-4 lg:gap-8 xl:gap-0 xl:px-0 lg:py-16 lg:flex lg:justify-between lg:items-center">
       <div className="mt-16 text-center lg:text-left">
-        <h3 className="max-w-3xl font-extrabold mb-4 text-6xl lg:text-6xl tracking-tight">{ welcomeMessage }, I'm <span className="text-alizarin">Soumyanil Das</span></h3>
+        {welcomeMessage ? <h3 className="max-w-3xl font-extrabold mb-4 text-6xl lg:text-6xl tracking-tight">{welcomeMessage}, I'm <span className="text-alizarin">Soumyanil Das</span></h3> : null}
         <p className="max-w-3xl font-light text-xl lg:text-3xl tracking-tight">A <span className="text-alizarin font-semibold">Calcutta</span> based Software Engineer, Frontend Developer and Air Guitarist</p>
       </div>
       <div className="mt-16 flex justify-center lg:mx-auto">
