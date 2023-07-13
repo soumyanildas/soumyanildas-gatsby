48711fe02fa6a3555b27f044ae86b19bc9773c98
Soumyanil Das <soumyanil666@gmail.com>
2023-07-13 19:57:36 +0530

    Added static folder

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="container mx-auto px-4 py-6 xl:px-0 flex justify-between items-center">
       <h1 className="text-alizarin font-bold text-5xl">sd.</h1>
       <ul className="flex justify-between items-center">
-        <li className="mr-4 hover:text-alizarin"><a href="https://res.cloudinary.com/soumyanil/image/upload/v1689257652/Resume-Soumyanil-Das.pdf" download>Resume</a></li>
+        <li className="mr-4 hover:text-alizarin"><a href="/downloads/Resume-Soumyanil-Das.pdf" download>Resume</a></li>
         <li><button onClick={() => scrollTo('#contact-us')} className="rounded-full outline outline-2 outline-alizarin py-2 px-6 text-alizarin transition-colors duration-300 ease-in-out hover:bg-alizarin hover:text-white">Say Hello</button></li>
       </ul>
     </header>
