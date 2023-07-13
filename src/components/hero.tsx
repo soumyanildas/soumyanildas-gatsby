928119a558bd2b8731fdf96f50a3bab5806e9711
Soumyanil Das <soumyanil666@gmail.com>
2023-07-13 17:38:33 +0530

    Using static welcome message

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,25 +2,10 @@ import React from 'react';
 
 const Hero = () => {
 
-  const [welcomeMessage, setWelcomeMessage] = React.useState('');
-
-  React.useEffect(() => {
-    generateGreetings();
-  }, []);
-
-  /**
-   * Generate random greetings
-   */
-  const generateGreetings = () => {
-    const greetings = ['Hello', 'Hallo', 'Bonjour', 'Olá', 'Hola', 'Namaste', 'Nomoshkar', 'Konnichiwa', 'Ciao'];
-    const randomIndex = Math.floor(Math.random() * greetings.length);
-    setWelcomeMessage(greetings[randomIndex]);
-  };
-
   return (
     <div className="container mx-auto px-4 lg:gap-8 xl:gap-0 xl:px-0 lg:py-16 lg:flex lg:justify-between lg:items-center">
       <div className="mt-16 text-center lg:text-left">
-        {welcomeMessage ? <h3 className="max-w-3xl font-extrabold mb-4 text-6xl lg:text-6xl tracking-tight">{welcomeMessage}, I'm <span className="text-alizarin">Soumyanil Das</span></h3> : null}
+        <h3 className="max-w-3xl font-extrabold mb-4 text-6xl lg:text-6xl tracking-tight">Hello, I'm <span className="text-alizarin">Soumyanil Das</span></h3>
         <p className="max-w-3xl font-light text-xl lg:text-3xl tracking-tight">A <span className="text-alizarin font-semibold">Calcutta</span> based Software Engineer, Frontend Developer and Air Guitarist</p>
       </div>
       <div className="mt-16 flex justify-center lg:mx-auto">
