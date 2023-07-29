cec8747649fd4afb03339965a5bbdb083d986ae1
Soumyanil Das <soumyanil666@gmail.com>
2023-07-29 17:19:26 +0530

    Reverted text, title and meta changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="container mx-auto px-4 lg:gap-8 xl:gap-0 xl:px-0 lg:py-16 lg:flex lg:justify-between lg:items-center">
         <div className="mt-16 text-center lg:text-left">
           <h3 className="max-w-3xl font-extrabold mb-4 text-5xl lg:text-7xl tracking-tight">Hello, I'm <span className="text-cyan-700">Soumyanil</span></h3>
-          <p className="max-w-3xl font-light text-xl lg:text-3xl tracking-tight">A <span className="text-cyan-700 font-semibold">Calcutta</span> based Software Engineer, Front-end Developer and Air Guitarist</p>
+          <p className="max-w-3xl font-light text-xl lg:text-3xl tracking-tight">A <span className="text-cyan-700 font-semibold">Calcutta</span> based Software Engineer, Frontend Developer and Air Guitarist</p>
         </div>
         <div className="mt-16 flex justify-center lg:mx-auto">
           <img height="300" width="300" src="/images/hero-das.svg" alt="hero-das" />
