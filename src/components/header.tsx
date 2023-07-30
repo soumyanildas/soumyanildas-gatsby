d84a3df1434a3e03dac6b8f434efa8a7005f0132
Soumyanil Das <soumyanil666@gmail.com>
2023-07-30 20:42:59 +0530

    Changed logo dimensions

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC<Props> = ({ children }) => {
   return (
     <header className="container mx-auto px-4 py-6 xl:px-0 flex justify-between items-center selection:bg-cyan-700 selection:text-white">
-      <img width="70" height="70" src="/images/logo-cyan.svg" alt="logo-cyan" />
+      <img width="60" height="60" src="/images/logo-cyan.svg" alt="logo-cyan" />
       {children}
     </header>
   );
